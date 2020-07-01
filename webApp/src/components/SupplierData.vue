5c4277c23eba337d5b3c83f4a434a71fdc70aa01
zengqingfu <48273213+zengqingfu@users.noreply.github.com>
2020-07-01 21:11:54 +0800

    供应商修正

--- webApp/src/components/SupplierData.vue
+++ webApp/src/components/SupplierData.vue
@@ -4,15 +4,15 @@
       进行中项目
     </h3>
 
-    <el-table @row-click="handle" :data="tableData" border :summary-method="jsondata.getSummaries" show-summary style="width: 100%">
-      <el-table-column prop="ContractDate" label="签约时间" sortable></el-table-column>
-      <el-table-column prop="projectName" label="项目名称" sortable></el-table-column>
-      <!-- <el-table-column prop="CustomerName" label="甲方" sortable></el-table-column> -->
-      <el-table-column prop="ContractAmount" label="项目金额" sortable></el-table-column>
-      <el-table-column prop="Receivables" label="已收金额" sortable></el-table-column>
-      <el-table-column prop="ExpenditureBudget" label="应付金额" sortable></el-table-column>
-      <el-table-column prop="expenditure" label="已付金额" sortable></el-table-column>
-      <el-table-column prop="projectContent" label="项目说明" sortable></el-table-column>
+    <el-table @row-click="handle" :data="fromexpenditure" border :summary-method="jsondata.getSummaries" show-summary style="width: 100%">
+      <el-table-column prop="SupplierName" label="供应商名称" sortable></el-table-column>
+      <el-table-column prop="projectId" label="项目名称" sortable></el-table-column>
+      <el-table-column prop="ReceivablesName" label="项目内容" sortable></el-table-column>
+      <el-table-column prop="number" label="项目金额" sortable></el-table-column>
+      <el-table-column prop="Receivables" label="已付金额" sortable></el-table-column>
+      <el-table-column prop="ExpenditureBudget" label="未付金额" sortable></el-table-column>
+      <el-table-column prop="projectClass" label="支出类别" sortable></el-table-column>
+      <el-table-column prop="contractdate" label="签约时间" sortable></el-table-column>
     </el-table>
   </el-main>
 </template>
@@ -35,7 +35,10 @@ export default {
         ContractDate: '',
         ExpenditureBudget: ''
       },
-      formexpenditureData: [],
+      fromexpenditureData: [],
+      fromprojectList: [],
+      fromexpenditure: [],
+      fromsupplierlist: [],
       formReceivables: []
     }
   },
@@ -48,7 +51,6 @@ export default {
   },
   mounted () {
     this.getdata()
-    this.getFormData('Customerlist')
   },
   methods: {
     submitForm (formName) {
@@ -81,21 +83,48 @@ export default {
     },
     getdata () {
       this.jsondata.getData('expenditureData').then(response => {
-        this.formexpenditureData = response.data
-      })
-        .catch(error => {
-          console.log(error)
+        this.fromexpenditureData = response.data
+        this.jsondata.getDataClass('projectList', '0', 'complete').then(response => {
+          this.fromprojectList = response.data
+          this.jsondata.getData('expenditure').then(response => {
+            for (let i = 0; i < this.fromprojectList.length; i++) {
+              for (let is = 0; is < response.data.length; is++) {
+                if(this.fromprojectList[i].id == response.data[is].projectId){ //eslint-disable-line
+                  this.fromexpenditure.push(response.data[is])
+                }
+              }
+            }
+            for (let i = 0; i < this.fromprojectList.length; i++) {
+              for (let is = 0; is < this.fromexpenditure.length; is++) {
+                if(this.fromprojectList[i].id == this.fromexpenditure[is].projectId){ //eslint-disable-line
+                  this.fromexpenditure[is].projectId = this.fromprojectList[i].projectName
+                }
+              }
+            }
+            this.jsondata.getData('supplierlist').then(response => { // 供应商例表
+              this.fromsupplierlist = response.data
+              for (let i = 0; i < this.fromexpenditure.length; i++) {
+                for (let is = 0; is < response.data.length; is++) {
+                  if(response.data[is].SupplierClass != '1' && this.fromexpenditure[i].SupplierName == response.data[is].id){ //eslint-disable-line
+                    this.fromexpenditure.splice(i, 1)
+                  }
+                  if(this.fromexpenditure[i].SupplierName == response.data[is].id){ //eslint-disable-line
+                    this.fromexpenditure[i].SupplierName = response.data[is].SupplierName
+                  }
+                }
+              }
+            })
+              .catch(error => {
+                console.log(error)
+              })
+          })
+            .catch(error => {
+              console.log(error)
+            })
         })
-      this.jsondata.getDataClass('projectList', '0', 'complete').then(response => {
-        this.tableData = response.data
-        // console.log(this.tableData)
-        for (let i = 0; i < this.tableData.length; i++) {
-          this.tableData[i].ContractDate = response.data[0].ContractDate.substr(0, 10)
-          this.tableData[i].ContractAmount = this.jsondata.currency(Number(this.tableData[i].ContractAmount), '￥', 2)
-          this.tableData[i].ExpenditureBudget = this.jsondata.currency(this.tableData[i].ExpenditureBudget, '￥', 2)
-          this.tableData[i].Receivables = 0
-        }
-        this.getdataReceivables()
+          .catch(error => {
+            console.log(error)
+          })
       })
         .catch(error => {
           console.log(error)
@@ -106,37 +135,6 @@ export default {
       this.$router.push('/ProjectDetails/' + row.id)
       console.log(row.id)
       // console.log(row, event, column)
-    },
-    getFormData (formName) { // 加载客户例表
-      this.jsondata.getData(formName).then(response => {
-        this.optionsproject = response.data
-      })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-    getdataReceivables () {
-      this.jsondata.getData('Receivables').then(response => {
-        for (var i = 0; i < this.tableData.length; i++) {
-          for (let iee in this.formexpenditureData) { //eslint-disable-line
-            if (this.tableData[i].id == this.formexpenditureData[iee].projectlist) { //eslint-disable-line
-              this.tableData[i].expenditure += Number(this.formexpenditureData[iee].Receivables)
-            }
-          }
-          for (let is = 0; is < response.data.length; is++) {
-            if(this.tableData[i].id == response.data[is].projectlist){ //eslint-disable-line
-              this.tableData[i].Receivables += Number(response.data[is].Receivables)
-              // console.log(this.tableData[i].Receivables, response.data[is], is)
-            }
-          }
-          this.tableData[i].Receivables = this.jsondata.currency(Number(this.tableData[i].Receivables), '￥', 2)
-          this.tableData[i].expenditure = this.jsondata.currency(Number(this.tableData[i].expenditure), '￥', 2)
-        }
-      })
-        .catch(error => {
-          console.log(error)
-        })
-      return false
     }
   }
 }
